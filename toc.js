126d47e5ef06dfb0d2d82cf0009b5e228d67f297
Pascal Hertleif <killercup@gmail.com>
2015-01-26 22:28:03 +0100

    Generated documentation for 802e54c09793a4a5544613981b927e5abae243aa

---
+++ toc.js
@@ -0,0 +1,27 @@
+window.files = [
+{
+  "path": "index.js.html",
+  "originalName": "index.js",
+  "originalPath": "index.js",
+  "name": "index.js.html",
+  "lang": "javascript",
+  "title": "Data Faking DSL",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "data-faking-dsl",
+      "title": "Data Faking DSL"
+    },
+    {
+      "level": 2,
+      "slug": "create-some-fakes",
+      "title": "Create Some Fakes"
+    },
+    {
+      "level": 2,
+      "slug": "remember-my-data",
+      "title": "Remember My Data"
+    }
+  ]
+}
+];
